de87819431b50f606476c2b8cae07fd26bb965e6
Ojas Nimase <40679873+ojuicen@users.noreply.github.com>
2025-10-25 15:18:30 -0700

    Delete script directory

--- script/ragroll.sh
+++
@@ -1,3 +0,0 @@
-export CUDA_VISIBLE_DEVICES=5
-# python -m experiment.main --mode=suffix --dataset=ragroll --catalog="air compressor"
-python -m experiment.main --mode=suffix --dataset=ragroll --catalog="paint sprayer"
--- script/transfer.sh
+++
@@ -1,2 +0,0 @@
-
-python -m experiment.transfer --base=mistral-7b
